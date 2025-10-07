5f7a831746ef609f1db8c75fb13f3347beeb5716
Winner Isac KODJO <147872844+winner-kw3@users.noreply.github.com>
2025-10-07 12:09:08 +0200

    Update liste_course.py

--- liste_course.py
+++ liste_course.py
@@ -14,5 +14,22 @@ print(mon_df)
 # Le gérant souhaite consulter les informations sur la laitue.
 print(mon_df.loc[mon_df['Produit'] == 'Laitue'])
 
+data_client = {
+    "Produit": ['Pomme', 'Fraise', 'Carotte'],
+    "Stock": [2, 5, 3],
+    "Prix Quantité": [0.8*2, 3.5*2, 1*3]
+}
+
+ticket_df = pandas.DataFrame(data_client)
+
+print(ticket_df)
+
+copi_stock_df = mon_df.copy()
+for produit, quantite in zip(ticket_df['Produit'], ticket_df['Stock']):
+    copi_stock_df.loc[copi_stock_df['Produit'] == produit, 'Stock'] -= quantite
+
+print("\nStock mis à jour après achat :")
+print(copi_stock_df)
+
 #Enregistrer le dataframe dans un fichier CSV
 mon_df.to_csv('liste_course.csv', index=False)
